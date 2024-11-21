d2e9b5968fea4aa6ac2fb69a101d96d293bf4461
Maihuy211 <maihuylc@gmail.com>
2024-11-21 15:42:09 +0700

    upload baitap2

---
+++ baitap2.c
@@ -0,0 +1,10 @@
+# include <stdio.h>
+int main ()
+{
+	int Celsius,Fahrenheit;
+	printf("nhap do Celsius: ");
+	scanf("%d",&Celsius);
+	Fahrenheit =((Celsius*9)/5)+32;
+	printf("do Fahrenheit la: %d",Fahrenheit);
+	return 0;
+	}
